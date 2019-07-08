ca2fc13f4ac435de11accfb1258e7bd8e69bb5b3
TristanR99 <48964179+TristanR99@users.noreply.github.com>
2019-07-08 19:06:00 -0400

    Reverses an array using two methods

--- Javascript/ReverseArray.js
+++ Javascript/ReverseArray.js
@@ -1,12 +1,12 @@
 //Reverses an array
-function reverseArray(array) {
+exports.reverseArray = function reverseArray(array) {
     let newArr = [];
     for(let element of array)
         newArr.unshift(element);
     return newArr;
 }
 
-function reverseInPlace(array) {
+exports.reverseInPlace = function reverseInPlace(array) {
     for(let x = 0, y = (array.length - 1); x <= Math.floor((array.length)/2) - 1; x++, y--){
        let temp = array[x];
        array[x] = array[y];
@@ -15,7 +15,5 @@ function reverseInPlace(array) {
         
     return array;
 }
-
-console.log(reverseInPlace([1,2,3,4,5,6,7,8,9]));
 
 
